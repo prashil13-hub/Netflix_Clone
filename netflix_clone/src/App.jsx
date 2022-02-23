f47513a9876462c4561700e7c2680b8997dedbe3
prashil13-hub <prashilganvir13@gmail.com>
2022-02-23 19:21:53 +0530

    Created register and login page

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.scss';
 import Home from './Pages/Home/Home';
 import Watch from './Pages/Watch/Watch';
+import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login';
+
 
 function App() {
   return (
-    <Home/>
+    // <Home/>
     // <Watch/>
+    // <Register/>
+    <Login/>
   );
 }
 
