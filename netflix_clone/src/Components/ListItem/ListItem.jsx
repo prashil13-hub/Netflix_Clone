6b9e68a3175062a24671281a64ce26fc3c1d0ea7
prashil13-hub <prashilganvir13@gmail.com>
2022-02-23 14:12:53 +0530

    Make UI for list Components

---
+++ netflix_clone/src/Components/ListItem/ListItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ListItem.scss'
+
+function ListItem() {
+  return (
+    <div className="listItem">
+
+    </div>
+  )
+}
+
+export default ListItem
--- netflix_clone/src/Home/Home.jsx
+++ netflix_clone/src/Home/Home.jsx
@@ -3,12 +3,18 @@ import AcUnitIcon from '@mui/icons-material/AcUnit';
 import './Home.scss'
 import Navbar from '../Components/Navbar/Navbar';
 import Featured from '../Components/Featured/Featured';
+import List from '../Components/List/List'
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
         <Featured type="movie"/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
     </div>
   )
 }
